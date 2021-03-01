bc3504e726971be4f4b28412983b5144ee1a98bb
antony-jr <antonyjr@protonmail.com>
2021-03-01 23:13:45 +0530

    Website: fix typo in versions page.

--- website/pages/en/versions.js
+++ website/pages/en/versions.js
@@ -23,7 +23,7 @@ function Versions(props) {
             <h1>{siteConfig.title} Versions</h1>
           </header>
           <h3 id="latest">Current version (Stable)</h3>
-          <p>Latest version of Docusaurus.</p>
+          <p>Latest version of QAppImageUpdate.</p>
           <table className="versions">
             <tbody>
               <tr>
@@ -62,7 +62,7 @@ function Versions(props) {
           </table>
           <h3 id="archive">Past Versions</h3>
           <p>
-            Here you can find documentation for previous versions of Docusaurus.
+            Here you can find documentation for previous versions of QAppImageUpdate.
           </p>
           <table className="versions">
             <tbody>
